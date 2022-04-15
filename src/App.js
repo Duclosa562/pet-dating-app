f7bed6e288af20b9664716a2e5b8449ed42198bb
Alexandre Duclos <duclosa@oregonstate.edu>
2022-04-15 09:13:45 -0700

    started the layout of the admin crud page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import Template from './Template';
+import React from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import './App.css';
+import HomePage from './pages/HomePage';
+import AdminCRUD from './pages/AdminCRUD';
+import { Link } from 'react-router-dom';
+import Navigation from './components/SiteNavLinks/Navigation.js';
+
 
 function App() {
   return (
     <div className="App">
-      <Template />
+      <header className="App-header"></header>
+      <Router>
+        <Routes>
+            <Route path="/" exact element={<HomePage/>}></Route>
+            <Route path="/AdminCRUD" element={<AdminCRUD/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
