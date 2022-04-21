d4b145bc7af42842e1e77341f41ec4d791d23da3
Calvin Hoo <hooc@oregonstate.edu>
2022-04-21 14:05:46 -0700

    Created Search Page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import AdminCRUD from './pages/AdminCRUD';
 import PetProfile from './pages/PetProfile';
+import SearchPage from './pages/SearchPage';
+
 import { Link } from 'react-router-dom';
 import Navigation from './components/SiteNavLinks/Navigation.js';
 
@@ -24,6 +26,7 @@ function App() {
             <Route path="/" exact element={<HomePage/>}></Route>
             <Route path="/AdminCRUD" element={<AdminCRUD/>}></Route>
             <Route path="/PetProfile" element={<PetProfile/>}></Route>
+			<Route path="/Search" element={<SearchPage/>}></Route>
         </Routes>
       </Router>
     </div>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -10,7 +10,8 @@ function HomePage() {
     <NavBar/>
     <p>TEST</p>
     <Link to="/AdminCRUD"><button>Admin_CRUD</button></Link >
-    <Link to="/PetProfile"><button>PetProfile</button></Link>  
+    <Link to="/PetProfile"><button>PetProfile</button></Link>
+    <Link to="/Search"><button>Search</button></Link>
     </div>
     );
 }
