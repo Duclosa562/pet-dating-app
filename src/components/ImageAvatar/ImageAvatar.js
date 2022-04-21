4a2672c5634eaa5a2a65943b20b819814b5b6eae
Calvin Hoo <hooc@oregonstate.edu>
2022-04-21 09:42:27 -0700

    Changed ImageAvatar to pass the image through props property.

--- src/components/ImageAvatar/ImageAvatar.js
+++ src/components/ImageAvatar/ImageAvatar.js
@@ -1,12 +1,11 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Stack from '@mui/material/Stack';
-import pokeImage from "../../images/fuecoco.png";
 
 function ImageAvatars(props) {
   return (
     <Stack direction="row" spacing={2}>
-      <Avatar alt="New Pet" src={pokeImage} sx={{ width: 256, height: 256 }}/>
+      <Avatar alt="New Pet" src={props.imagePath} sx={{ width: 256, height: 256 }}/>
     </Stack>
   );
 }
--- src/pages/PetProfile.js
+++ src/pages/PetProfile.js
@@ -1,13 +1,24 @@
-import Avatar from '@mui/material/Avatar';
+import Grid from '@mui/material/Grid';
+
 import ImageAvatar from '../components/ImageAvatar/ImageAvatar';
 import NavBar from '../components/NavBar/NavBar';
+import ProfileCard from '../components/ProfileCard/ProfileCard';
+
+import pokeImage from "../images/fuecoco.png";
+
+import "../styles/PetProfile.css";
 
 function PetProfile() {
     return (
         <div className="pet-profile">
             <NavBar />
-            <Avatar alt='Fuecoco' src= './images/fuecoco.png'/>
-            <ImageAvatar />
+            <div className="pet-data">
+                <Grid container columns={2} direction='row' alignItems='center' justifyContent='center'>
+                    <ImageAvatar imagePath={pokeImage} />
+                    <ProfileCard />
+                </Grid>
+            </div>
+            
         </div>
     )
 }
