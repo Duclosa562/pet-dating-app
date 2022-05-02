b6b14ff37af149af0cbc487283b8841f3e0ba405
Alexandre Duclos <duclosa@oregonstate.edu>
2022-05-02 09:06:15 -0700

    added a handler to server.js for heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -119,6 +119,22 @@ router.put('/api/update', async function(req, res) {
     res.status(200).send({"data": data});
 });
 
+/*****************************
+ * TO FIX HEROKU DEPLOY ISSUE (ALEX)
+ * NOTE: NODE_ENV might need to change to work on production
+ *****************************/
+
+if (process.env.NODE_ENV === 'develop') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+  
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+  }
+
 app.use(router);
 
 const port = process.env.PORT || 5000;
