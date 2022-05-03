2a09747c440b62985471e4d0cfadf3089244792d
Spencer Neukam <neukams@oregonstate.edu>
2022-05-03 11:04:30 -0600

    modified server.js with routes for build file to be handled by server.

--- backend/server.js
+++ backend/server.js
@@ -136,6 +136,12 @@ if (process.env.NODE_ENV === 'production'){
 });
 }*/
 
+// Heroku guide
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+});
+
 
 app.use(router);
 
