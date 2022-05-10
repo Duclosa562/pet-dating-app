44774be46e3f04986535255e9034b015a75ac7df
Spencer Neukam <neukams@oregonstate.edu>
2022-05-10 17:25:15 -0600

    modified file to get build right?

--- src/config.js
+++ src/config.js
@@ -1,29 +1,36 @@
-var config = {};
 
+function configureConfigThing() {
 
-// Set Proxy URL
-if (!process.env.PROXY_URL) {
-    console.log('Loaded .env = PROXY_URL');
-    config.PROXY_URL = 'http://localhost:5000';
-} else {
-    config.PROXY_URL = process.env.PROXY_URL;
+    var config = {}
+
+    // Set Proxy URL
+    if (!process.env.PROXY_URL) {
+        console.log('Loaded .env = PROXY_URL');
+        config.PROXY_URL = 'http://localhost:5000';
+    } else {
+        config.PROXY_URL = process.env.PROXY_URL;
+    }
+
+    // Set DB Instance
+    if (!process.env.DB_INSTANCE) {
+        console.log('Loaded .env = DB_INSTANCE');
+        config.DB_INSTANCE = 'PetDatingApp-Local';
+    } else {
+        config.DB_INSTANCE = process.env.DB_INSTANCE;
+    }
+
+    // Set MongoDB Instance
+    if (!process.env.MONGODB_URI) {
+        console.log('Loaded .env = MONGODB_URI');
+        config.MONGODB_URI = 'mongodb+srv://admin:QmEAuuqPj9qEJDkBt@cluster0.9a9u5.mongodb.net/test?retryWrites=true&w=majority';
+    } else {
+        config.MONGODB_URI = process.env.MONGODB_URI;
+    }
+
+    return config;
 }
 
-// Set DB Instance
-if (!process.env.DB_INSTANCE) {
-    console.log('Loaded .env = DB_INSTANCE');
-    config.DB_INSTANCE = 'PetDatingApp-Local';
-} else {
-    config.DB_INSTANCE = process.env.DB_INSTANCE;
-}
-
-// Set MongoDB Instance
-if (!process.env.MONGODB_URI) {
-    console.log('Loaded .env = MONGODB_URI');
-    config.MONGODB_URI = 'mongodb+srv://admin:QmEAuuqPj9qEJDkBt@cluster0.9a9u5.mongodb.net/test?retryWrites=true&w=majority';
-} else {
-    config.MONGODB_URI = process.env.MONGODB_URI;
-}
+var config = configureConfigThing();
 
 module.exports = {
     config
