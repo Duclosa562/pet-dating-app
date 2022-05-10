ad0ad09791450e56dcd8ccdb4f22d879cee7894b
Spencer Neukam <neukams@oregonstate.edu>
2022-05-09 21:31:16 -0600

    removed commented out code that we did not need

--- src/components/UploadPhotoButton/UploadPhoto.js
+++ src/components/UploadPhotoButton/UploadPhoto.js
@@ -9,69 +9,6 @@ const Input = styled('input')({
   display: 'none',
 });
 
-// KEEP THIS CODE EVEN THOUGH COMMENTED OUT
-// SOURCE: https://codesandbox.io/s/convert-file-to-base64-in-react-lqi1e?file=/src/App.js:118-1105
-
-/*var file;
-var base64URL;
-
-var state = {
-  file: null,
-  base64URL: ""
-};
-
-// SOURCE: https://codesandbox.io/s/convert-file-to-base64-in-react-lqi1e?file=/src/App.js:118-1105
-
-getBase64 = file => {
-  return new Promise(resolve => {
-    let fileInfo;
-    let baseURL = "";
-    // Make new FileReader
-    let reader = new FileReader();
-
-    // Convert the file to base64 text
-    reader.readAsDataURL(file);
-
-    // on reader load somthing...
-    reader.onload = () => {
-      // Make a fileInfo Object
-      console.log("Called", reader);
-      baseURL = reader.result;
-      console.log(baseURL);
-      resolve(baseURL);
-    };
-    console.log(fileInfo);
-  });
-};
-
-// SOURCE: https://codesandbox.io/s/convert-file-to-base64-in-react-lqi1e?file=/src/App.js:118-1105
-
-handleFileInputChange = e => {
-  console.log(e.target.files[0]);
-  let { file } = this.state;
-
-  file = e.target.files[0];
-
-  this.getBase64(file)
-    .then(result => {
-      file["base64"] = result;
-      console.log("File Is", file);
-      this.setState({
-        base64URL: result,
-        file
-      });
-    })
-    .catch(err => {
-      console.log(err);
-    });
-
-  this.setState({
-    file: e.target.files[0]
-  });
-};
-
-var imageBase64 = '';*/
-
 function getBase64(file) {
   var reader = new FileReader();
   reader.readAsDataURL(file);
