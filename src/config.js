897590d656e1763b65458b2b6bda6bc837080a8c
Spencer Neukam <neukams@oregonstate.edu>
2022-05-10 16:35:09 -0600

    config changes to load env variables correctly on Heroku.

--- src/config.js
+++ src/config.js
@@ -1,11 +1,27 @@
 var config = {};
 
-config.PROXY_URL = process.env.PROXY_URL || 'http://localhost:5000';
-config.DB_INSTANCE = process.env.DB_INSTANCE || 'PetDatingApp-Local';
-config.MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://admin:QmEAuuqPj9qEJDkBt@cluster0.9a9u5.mongodb.net/test?retryWrites=true&w=majority';
+
+// Set Proxy URL
+if (!process.env.PROXY_URL) {
+    config.PROXY_URL = 'http://localhost:5000';
+} else {
+    config.PROXY_URL = process.env.PROXY_URL;
+}
+
+// Set DB Instance
+if (!process.env.DB_INSTANCE) {
+    config.DB_INSTANCE = 'PetDatingApp-Local';
+} else {
+    config.DB_INSTANCE = process.env.DB_INSTANCE;
+}
+
+// Set MongoDB Instance
+if (!process.env.MONGODB_URI) {
+    config.MONGODB_URI = 'mongodb+srv://admin:QmEAuuqPj9qEJDkBt@cluster0.9a9u5.mongodb.net/test?retryWrites=true&w=majority';
+} else {
+    config.MONGODB_URI = process.env.MONGODB_URI;
+}
 
 module.exports = {
     config
 };
-//
-//
