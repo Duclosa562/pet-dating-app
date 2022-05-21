c0a5adbbdd3d59bc488e2a74f4aaf8014ff08598
Spencer Neukam <neukams@oregonstate.edu>
2022-05-21 15:16:43 -0600

    Pet Profile -> 'Adopt' button conditionally clickabe/unclickable based on the pets availability. It does not yet redirect to another page to adopt the pet.

--- src/components/ProfileCard/ProfileCard.js
+++ src/components/ProfileCard/ProfileCard.js
@@ -5,7 +5,8 @@ import CardActions from '@mui/material/CardActions';
 import Button from '@mui/material/Button';
 
 import './ProfileCard.css';
-import { CardMedia } from '@mui/material';
+import {queries} from '../../utils/queries';
+
 
 function bools_to_strings(pet) {
     for (var key in pet) {
@@ -19,9 +20,17 @@ function bools_to_strings(pet) {
     }
 }
 
+function button_html(pet) {
+    if (pet.availability != 'Available') {
+        return <Button variant='contained' disabled>ADOPT</Button>
+    }
+    return <Button variant='contained'>ADOPT</Button>
+}
+
 function ProfileCard({petData}) {
     var petInfo = petData.data;
     bools_to_strings(petInfo);
+    const button = button_html(petInfo);
     return (
         <div className='profile-card'>
             <Card sx={{width: 400, background: "rgba(255, 255, 255, 1.0)", m: "20px"}}>
@@ -29,18 +38,17 @@ function ProfileCard({petData}) {
                 <CardContent classes='pet-content'> 
                     <div className='card-content'>
                         <p><b>Age: </b>{petInfo.age} {petInfo.age_descriptor}</p> 
-                        <p><b>Status: </b>{petInfo.availability}</p>
                         <p><b>Date Added: </b>{petInfo.date_created}</p>
                         <p><b>Good with animals: </b>{String(petInfo.good_with_animals)}</p>
                         <p><b>Good with Children: </b>{String(petInfo.good_with_children)}</p>
                         <p><b>Must be leashed: </b>{String(petInfo.must_be_leashed)}</p>
                         <p><b>Description: </b>{petInfo.description}</p>
+                        <p><b>Status: </b>{petInfo.availability}</p>
                     </div>
                 </CardContent>
                 <CardActions>
-                    <Button variant='contained'>ADOPT</Button>
+                    {button}
                 </CardActions>
-                
             </Card>
         </div>
     )
