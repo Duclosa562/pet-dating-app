3f410ba4c70ac77caf74c3268eb71dedf396107d
Alexandre Duclos <duclosa@oregonstate.edu>
2022-05-27 09:44:42 -0700

    modified test app.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/PetProfile/i);
+  const linkElement = screen.getByText(/Pet Dating App/i);
   expect(linkElement).toBeInTheDocument();
 });
