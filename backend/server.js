17edd9cd92154017a57bdc1e13ac7c8a34713320
Spencer Neukam <neukams@oregonstate.edu>
2022-05-28 07:52:44 -0600

    Uploading a very large photo for a new animal's profile is fixed. Limit is now 50mb

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,8 @@ const { MongoClient, ObjectId } = require("mongodb");
 const fs = require('fs');
 
 app.enable('trust proxy');
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }));
 app.use(cors());
 const router = express.Router()
 
